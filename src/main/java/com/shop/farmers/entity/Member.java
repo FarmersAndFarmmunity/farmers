ba1b33d2d1314b00dac602c8337635b16a8800bb
whale22 <realwhale64@naver.com>
2023-05-29 20:53:30 +0900

    fear: nav바 추가를 위한 hearder.html 수정

--- src/main/java/com/shop/farmers/entity/Member.java
+++ src/main/java/com/shop/farmers/entity/Member.java
@@ -3,11 +3,10 @@ package com.shop.farmers.entity;
 import com.shop.farmers.constant.Role;
 import com.shop.farmers.dto.MemberFormDto;
 import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,10 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SuperBuilder
 @ToString(callSuper = true)
 @NoArgsConstructor
-public class Member extends BaseEntity {
+public class Member extends BaseEntity{
     @Id
     @Column(name = "member_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
@@ -42,14 +41,12 @@ public class Member extends BaseEntity {
     private Role role;
 
     public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
-        Member member = Member.builder()
+        return Member.builder()
                 .name(memberFormDto.getName())
                 .email(memberFormDto.getEmail())
                 .address(memberFormDto.getAddress())
                 .password(passwordEncoder.encode(memberFormDto.getPassword()))
                 .role(Role.USER)
                 .build();
-
-        return member;
     }
 }
