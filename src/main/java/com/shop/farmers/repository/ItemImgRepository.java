8953258c23b34438864cd1be1822818bbddc3c07
duswnsxnxn <duswnsxnxn@gmail.com>
2023-05-30 22:47:34 +0900

    feat: 아이템 이미지 관련 기능 구현

---
+++ src/main/java/com/shop/farmers/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.shop.farmers.repository;
+
+import com.shop.farmers.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
