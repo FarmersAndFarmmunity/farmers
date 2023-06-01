e328c1d5cc9b4c39f410d4cdee77cbea2465c8b0
youngjae459 <youngjae459@gmail.com>
2023-06-01 15:39:26 +0900

    fix: 중복 코드 제거

--- src/main/java/com/shop/farmers/repository/ItemImgRepository.java
+++ src/main/java/com/shop/farmers/repository/ItemImgRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
-    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
