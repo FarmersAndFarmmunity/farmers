a9e07c3657d261b7129d5a64933730ca250f70f6
youngjae459 <youngjae459@gmail.com>
2023-06-08 17:57:29 +0900

    feat: 리뷰엔티티에 필드 초기화를 위해 dto 에 필드 추가

--- src/main/java/com/shop/farmers/boundedContext/review/dto/ReviewFormDto.java
+++ src/main/java/com/shop/farmers/boundedContext/review/dto/ReviewFormDto.java
@@ -1,20 +1,13 @@
 package com.shop.farmers.boundedContext.review.dto;
 
-import com.shop.farmers.boundedContext.item.constant.ItemSellStatus;
-import com.shop.farmers.boundedContext.item.dto.ItemImgDto;
 import com.shop.farmers.boundedContext.item.entity.Item;
 import com.shop.farmers.boundedContext.member.entity.Member;
-import jakarta.validation.constraints.Min;
+import com.shop.farmers.boundedContext.review.entity.Review;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.modelmapper.ModelMapper;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Getter
 @Setter
@@ -22,8 +15,18 @@ public class ReviewFormDto {
 
     private Long id;
 
+    private Member member;
+
+    private Item item;
+
     @Length(min=20, max=200, message = "리뷰는 최소 20자 이상 입력하셔야 합니다.")
     @NotBlank(message = "리뷰 내용은 필수 입력 값입니다.")
     private String contents;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public Review createReview() {
+        return modelMapper.map(this, Review.class);
+    }
 
 }
