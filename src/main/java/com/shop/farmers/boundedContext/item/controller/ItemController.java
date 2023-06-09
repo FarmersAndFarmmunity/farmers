90084fbd153febf3989004f8648e80e4fea954dd
youngjae459 <youngjae459@gmail.com>
2023-06-09 14:48:41 +0900

    feat: 리뷰를 잘 가져오는지 test

--- src/main/java/com/shop/farmers/boundedContext/item/controller/ItemController.java
+++ src/main/java/com/shop/farmers/boundedContext/item/controller/ItemController.java
@@ -124,6 +124,8 @@ public class ItemController {
     @GetMapping(value = "/item/{itemId}")
     public String itemDtl(Model model, @PathVariable Long itemId) {
         ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+
+
         model.addAttribute("item", itemFormDto);
         return "item/itemDtl";
     }
--- src/main/java/com/shop/farmers/boundedContext/review/repository/ReviewRepository.java
+++ src/main/java/com/shop/farmers/boundedContext/review/repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package com.shop.farmers.boundedContext.review.repository;
 import com.shop.farmers.boundedContext.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByItemIdLike(Long itemId);
 }
